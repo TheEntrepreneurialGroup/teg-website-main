8a8928fb7d09f04537c9edbde026646e065abc8b
Jonathan Babelotzky <you@example.com>
2025-05-03 10:47:18 +0200

    adding intl provider to app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import ForStudents from './pages/ForStudents';
-import ForCompanies from './pages/ForCompanies';
-import { Home } from './pages/Home';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import Imprint from './pages/Imprint';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import ForStudents from "./pages/ForStudents";
+import ForCompanies from "./pages/ForCompanies";
+import { Home } from "./pages/Home";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Imprint from "./pages/Imprint";
+import { IntlProvider } from "react-intl";
+import en from "./locales/en";
+import de from "./locales/de";
+
+const messages: Record<string, Record<string, string>> = { en, de };
 
 function App() {
+  const [locale, setLocale] = useState<"en" | "de">("en");
+
+  const switchLanguage = (lang: "en" | "de") => {
+    setLocale(lang);
+  };
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="for-students" element={<ForStudents />} />
-        <Route path="for-companies" element={<ForCompanies />} />
-      </Route>
-      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      <Route path="/imprint" element={<Imprint />} />
-    </Routes>
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="for-students" element={<ForStudents />} />
+          <Route path="for-companies" element={<ForCompanies />} />
+        </Route>
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/imprint" element={<Imprint />} />
+      </Routes>
+    </IntlProvider>
   );
 }
 
