f567a58f587c62ab5197eadd510a2f1601edad8f
jishnunambiarr <jishnunambiar03@gmail.com>
2025-06-14 23:23:41 +0200

    Finishing touches for next launch

--- src/components/HeroSectionTwoButtons.tsx
+++ src/components/HeroSectionTwoButtons.tsx
@@ -22,73 +22,71 @@ const HeroSectionTwoButtons: React.FC<HeroSectionTwoButtonsProps> = ({
 }) => {
   return (
     <div
-      className="relative h-screen lg:h-[96vh] min-h-[600px] flex items-center justify-start overflow-hidden"
+      className="relative h-screen lg:h-[108vh] min-h-[600px] flex items-center justify-start overflow-hidden"
       style={{
-        marginTop: "1rem",
-        backgroundImage: `linear-gradient(rgba(15, 44, 89, 0.8), rgba(15, 44, 89, 0.9)), url(${backgroundImage})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
+      marginTop: "1rem",
+      backgroundImage: `linear-gradient(rgba(15, 44, 89, 0.4), rgba(15, 44, 89, 0.5)), url(${backgroundImage})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
       }}
     >
-      <div className="absolute inset-0 bg-gradient-to-b from-primary/30 to-primary/80"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-primary/20 to-primary/60"></div>
       <div className="container-custom z-10 text-left pl-8">
-        <motion.h1
-          className="text-white mb-6 font-bold"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, delay: 0.2 }}
-        >
-          {title}
-        </motion.h1>
-        <motion.p
-          className="text-secondary-light text-xl max-w-4xl mb-8"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, delay: 0.4 }}
-        >
-          {subtitle}
-        </motion.p>
-        <motion.div
-          className="flex space-x-2"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, delay: 0.6 }}
-        >
-          {buttonText1 && buttonLink1 && (
-            //className="btn btn-primary border border-white"
-            <Button href={buttonLink1}>{buttonText1}</Button>
-          )}
-          {buttonText2 && buttonLink2 && (
-            //className="btn btn-primary border border-white"
-            <Button href={buttonLink2}>{buttonText2}</Button>
-          )}
-        </motion.div>
+      <motion.h1
+        className="text-white mb-6 font-bold"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.2 }}
+      >
+        {title}
+      </motion.h1>
+      <motion.p
+        className="text-secondary-light text-xl max-w-4xl mb-8"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
+      >
+        {subtitle}
+      </motion.p>
+      <motion.div
+        className="flex space-x-2"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.6 }}
+      >
+        {buttonText1 && buttonLink1 && (
+        <Button href={buttonLink1}>{buttonText1}</Button>
+        )}
+        {buttonText2 && buttonLink2 && (
+        <Button href={buttonLink2}>{buttonText2}</Button>
+        )}
+      </motion.div>
       </div>
       <motion.div
-        className="absolute bottom-10 left-1/2 transform -translate-x-1/2"
-        initial={{ opacity: 0.6, y: -4 }}
-        animate={{ opacity: 0.8, y: 0 }}
-        transition={{
-          duration: 0.6,
-          delay: 0.8,
-          repeat: Infinity,
-          repeatType: "reverse",
-        }}
+      className="absolute bottom-10 left-1/2 transform -translate-x-1/2"
+      initial={{ opacity: 0.6, y: -4 }}
+      animate={{ opacity: 0.8, y: 0 }}
+      transition={{
+        duration: 0.6,
+        delay: 0.8,
+        repeat: Infinity,
+        repeatType: "reverse",
+      }}
       >
-        <svg
-          className="w-6 h-6 text-white"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M19 14l-7 7m0 0l-7-7m7 7V3"
-          ></path>
-        </svg>
+      <svg
+        className="w-6 h-6 text-white"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M19 14l-7 7m0 0l-7-7m7 7V3"
+        ></path>
+      </svg>
       </motion.div>
     </div>
   );
