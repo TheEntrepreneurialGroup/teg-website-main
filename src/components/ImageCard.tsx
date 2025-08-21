26c8da866495775eb7ef4ae4931fcc8bbcdf82a7
Luis Waller <luis.waller@tum.de>
2025-08-21 16:10:14 +0200

    add: crop card images correctly + jonathan quote name without ~

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,39 +1,46 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
 interface ImageCardProps {
-    imageUrl: string;
-    altText: string;
-    caption?: string;
-    delay?: number;
-    className?: string;
+  imageUrl: string;
+  altText: string;
+  caption?: string;
+  delay?: number;
+  className?: string;
 }
 
-const ImageCard: React.FC<ImageCardProps> = ({ imageUrl, altText, caption, delay = 0, className }) => {
-    const combinedClassName = `relative overflow-hidden card ${className || ''}`;
+const ImageCard: React.FC<ImageCardProps> = ({
+  imageUrl,
+  altText,
+  caption,
+  delay = 0,
+  className,
+}) => {
+  const combinedClassName = `relative overflow-hidden card w-full${
+    className || ""
+  }`;
 
-    return (
-        <motion.div 
-            className={combinedClassName}
-            initial={{ opacity: 0, y: 20 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.6, delay }}
-        >
-           
-               <img 
-                    src={imageUrl} 
-                    alt={altText} 
-                    className="w-full h-auto object-cover" 
-                />
-        
-        {caption && (
-                 <div className="mt-2 px-4">
-                    <p className="text-lg leading-6 font-bold">{caption}</p>
-                 </div>
-            )}
-        </motion.div>
-    );
+  return (
+    <motion.div
+      className={combinedClassName}
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.6, delay }}
+    >
+      <img
+        src={imageUrl}
+        alt={altText}
+        className="w-full object-cover aspect-[948/695]"
+      />
+
+      {caption && (
+        <div className="mt-2 px-4">
+          <p className="text-lg leading-6 font-medium">{caption}</p>
+        </div>
+      )}
+    </motion.div>
+  );
 };
 
 export default ImageCard;
