c6f9e68466a3eb4ede6cdc4006e17e5caa2e14e2
Luis Waller <luis.waller@tum.de>
2025-08-21 09:07:28 +0200

    add: Scroll save + back to top when switching page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Imprint from "./pages/Imprint";
 import { IntlProvider } from "react-intl";
 import en from "./locales/en";
 import de from "./locales/de";
+import ScrollRestoration from "./components/ScrollRestoration";
 
 const messages: Record<string, Record<string, string>> = { en, de };
 
@@ -20,6 +21,7 @@ function App() {
   };
   return (
     <IntlProvider locale={locale} messages={messages[locale]}>
+      <ScrollRestoration />
       <Routes>
         <Route path="/" element={<Layout switchLanguage={switchLanguage} />}>
           <Route index element={<Home />} />
