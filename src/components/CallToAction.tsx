293de36704cbcaa5a52d82578915a4f6adfa2335
Jishnu Nambiar <jishnunambiar03@gmail.com>
2025-08-24 17:03:24 +0200

    alignment issue fixed, moved text outside the list in the bottom.

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -75,7 +75,9 @@ const CallToAction: React.FC<CallToActionProps> = ({
         isLight ? "bg-secondary-light" : "bg-primary"
       } ${className}`}
     >
-      {content}
+      <div className="text-center">
+        {content}
+      </div>
     </div>
   );
 };
