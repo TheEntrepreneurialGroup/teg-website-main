70d333afb59a32c7c8d42021bc1e0a83d68dfe7b
Luis Waller <luis.waller@tum.de>
2025-09-19 16:29:41 +0200

    add: header update

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -3,8 +3,7 @@ const en = {
   "home.hero.title":
     "We are developing the future leaders of the German economy.",
   "home.hero.since": "Since 1986.",
-  "home.hero.subtitle":
-    "The Entrepreneurial Group (TEG) shapes the next leaders of the German economy. We develop exceptional talents into entrepreneurs: founders, executives, and business leaders.",
+  //"home.hero.subtitle": "",
   "home.hero.buttonText1": "TEG for Companies",
   "home.hero.buttonLink1": "/for-companies",
   "home.hero.buttonText2": "TEG for Students",
