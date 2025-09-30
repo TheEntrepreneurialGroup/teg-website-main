d429126c35df770a889da9a58df26a9bbc595609
Luis Waller <luis.waller@tum.de>
2025-09-30 10:59:48 +0200

    add: Header update

---
+++ src/components/sections/HeroSectionStudents.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HeroSectionStudents: React.FunctionComponent = ({}) => {
+  return (
+    <div className="relative h-[100dvh]">
+      <div className="overflow-hidden">
+        <img
+          src="/for_students.jpg"
+          alt="Students"
+          className="object-contain"
+        ></img>
+      </div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default HeroSectionStudents;
