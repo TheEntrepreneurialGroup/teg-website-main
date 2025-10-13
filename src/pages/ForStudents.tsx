45218f6db149657cb45d7f7ec5ee39f0bd2a7c1e
Jonathan Babelotzky <you@example.com>
2025-10-13 22:23:45 +0200

    edit: max-width for whole for-students-page content

--- src/pages/ForStudents.tsx
+++ src/pages/ForStudents.tsx
@@ -37,124 +37,126 @@ const ForStudents: React.FC = () => {
         backgroundSize="cover"
         backgroundPosition="center top"
       />
-      {/* Commitment CardText Section */}
-      <CardText
-        image="/components-images/teg_alumni.png"
-        imageAlt={intl.formatMessage({ id: "student.commitment.imageAlt" })}
-        title={intl.formatMessage({ id: "student.commitment.title" })}
-        subtitle={intl.formatMessage({ id: "student.commitment.subtitle" })}
-      />
-
-      <LogoSection
-        images={[
-          { image: "/logos/bmw-image.webp", text: "BMW" },
-          { image: "/logos/bcg.png", text: "BCG" },
-          { image: "/logos/siemens.svg", text: "Siemens" },
-          {
-            image: "/logos/HypoVereinsbank.svg",
-            text: "HypoVereinsbank",
-          },
-          {
-            image: "/logos/Roland_Berger_logo.svg",
-            text: "Roland Berger",
-          },
-          { image: "/logos/ruhrgas-logo.png", text: "Ruhrgas" },
-        ]}
-        title={intl.formatMessage({ id: "home.partners.title" })}
-        description={intl.formatMessage({ id: "home.partners.description" })}
-      />
-
-      <CardText
-        image="/welcome-to-teg.jpeg"
-        imageAlt={intl.formatMessage({ id: "student.cardText.imageAlt" })}
-        title={intl.formatMessage({ id: "student.cardText.title" })}
-        subtitle={intl.formatMessage({ id: "student.cardText.subtitle" })}
-        body={intl.formatMessage({ id: "student.cardText.body" })}
-      />
-      <LogoSection
-        images={[
-          { image: "/logos/airbus.svg", text: "Airbus" },
-          {
-            image: "/logos/maiborn-logo.png",
-            text: "MaibornWolff",
-          },
-          { image: "/logos/tuv-logo.png", text: "TÜV" },
-          { image: "/logos/Deloitte.svg", text: "Deloitte" },
-          { image: "/logos/ey-logo.png", text: "EY" },
-          { image: "/logos/Vattenfall.svg", text: "Vattenfall" },
-        ]}
-        title={intl.formatMessage({ id: "home.alumnis.title" })}
-        description={intl.formatMessage({ id: "home.alumnis.description" })}
-      />
-
-      <CardText
-        image="/components-images/team-2.jpeg"
-        imageAlt={intl.formatMessage({ id: "student.cardText3.imageAlt" })}
-        title={intl.formatMessage({ id: "student.cardText3.title" })}
-        subtitle={intl.formatMessage({ id: "student.cardText3.subtitle" })}
-        body={intl.formatMessage({ id: "student.cardText3.body" })}
-      />
-
-      <CardText
-        image="/components-images/team-4.jpeg"
-        imageAlt={intl.formatMessage({ id: "student.cardText2.imageAlt" })}
-        title={intl.formatMessage({ id: "student.cardText2.title" })}
-        subtitle={intl.formatMessage({ id: "student.cardText2.subtitle" })}
-      />
-
-      <ApplicationPipelineSection />
-
-      <MemberProcessSection />
-
-      <div className="container-custom max-w-7xl mx-auto px-1 md:px-4 pb-8">
-        <CallToAction
-          title={intl.formatMessage({ id: "student.callToAction.title" })}
-          description={intl.formatMessage({
-            id: "student.callToAction.description",
-          })}
-          buttonText={intl.formatMessage({
-            id: "student.callToAction.buttonText",
-          })}
-          buttonLink={intl.formatMessage({
-            id: "student.callToAction.buttonLink",
-          })}
-          isSection={false}
+      <div className="max-w-7xl mx-auto">
+        {/* Commitment CardText Section */}
+        <CardText
+          image="/components-images/teg_alumni.png"
+          imageAlt={intl.formatMessage({ id: "student.commitment.imageAlt" })}
+          title={intl.formatMessage({ id: "student.commitment.title" })}
+          subtitle={intl.formatMessage({ id: "student.commitment.subtitle" })}
         />
-      </div>
 
-      <GoogleMaps />
+        <LogoSection
+          images={[
+            { image: "/logos/bmw-image.webp", text: "BMW" },
+            { image: "/logos/bcg.png", text: "BCG" },
+            { image: "/logos/siemens.svg", text: "Siemens" },
+            {
+              image: "/logos/HypoVereinsbank.svg",
+              text: "HypoVereinsbank",
+            },
+            {
+              image: "/logos/Roland_Berger_logo.svg",
+              text: "Roland Berger",
+            },
+            { image: "/logos/ruhrgas-logo.png", text: "Ruhrgas" },
+          ]}
+          title={intl.formatMessage({ id: "home.partners.title" })}
+          description={intl.formatMessage({ id: "home.partners.description" })}
+        />
 
-      <PyramideSection />
+        <CardText
+          image="/welcome-to-teg.jpeg"
+          imageAlt={intl.formatMessage({ id: "student.cardText.imageAlt" })}
+          title={intl.formatMessage({ id: "student.cardText.title" })}
+          subtitle={intl.formatMessage({ id: "student.cardText.subtitle" })}
+          body={intl.formatMessage({ id: "student.cardText.body" })}
+        />
+        <LogoSection
+          images={[
+            { image: "/logos/airbus.svg", text: "Airbus" },
+            {
+              image: "/logos/maiborn-logo.png",
+              text: "MaibornWolff",
+            },
+            { image: "/logos/tuv-logo.png", text: "TÜV" },
+            { image: "/logos/Deloitte.svg", text: "Deloitte" },
+            { image: "/logos/ey-logo.png", text: "EY" },
+            { image: "/logos/Vattenfall.svg", text: "Vattenfall" },
+          ]}
+          title={intl.formatMessage({ id: "home.alumnis.title" })}
+          description={intl.formatMessage({ id: "home.alumnis.description" })}
+        />
 
-      <CertificatesSection />
+        <CardText
+          image="/components-images/team-2.jpeg"
+          imageAlt={intl.formatMessage({ id: "student.cardText3.imageAlt" })}
+          title={intl.formatMessage({ id: "student.cardText3.title" })}
+          subtitle={intl.formatMessage({ id: "student.cardText3.subtitle" })}
+          body={intl.formatMessage({ id: "student.cardText3.body" })}
+        />
 
-      <Testimonials />
+        <CardText
+          image="/components-images/team-4.jpeg"
+          imageAlt={intl.formatMessage({ id: "student.cardText2.imageAlt" })}
+          title={intl.formatMessage({ id: "student.cardText2.title" })}
+          subtitle={intl.formatMessage({ id: "student.cardText2.subtitle" })}
+        />
 
-      <section id="contact" className="pt-20 bg-primary text-white">
-        <div className="flex flex-col items-center">
-          <div className="container mx-auto px-4 max-w-6xl">
-            {/* Section Title */}
-            <h2 className="text-3xl md:text-4xl font-bold mb-12 text-white text-center">
-              {intl.formatMessage({ id: "student.contact.title" })}
-            </h2>
+        <ApplicationPipelineSection />
 
-            {/* Card Container - Centered */}
-            <div className="flex justify-center">
-              <ContactCard
-                name={contactPerson.name}
-                title={contactPerson.title}
-                imageUrl={contactPerson.imageUrl}
-                email={contactPerson.email}
-                linkedinUrl={contactPerson.linkedinUrl}
-                greeting={intl.formatMessage({
-                  id: "student.contact.greeting",
-                })}
-                variant="quote"
-              />
+        <MemberProcessSection />
+
+        <div className="container-custom max-w-7xl mx-auto px-1 md:px-4 pb-8">
+          <CallToAction
+            title={intl.formatMessage({ id: "student.callToAction.title" })}
+            description={intl.formatMessage({
+              id: "student.callToAction.description",
+            })}
+            buttonText={intl.formatMessage({
+              id: "student.callToAction.buttonText",
+            })}
+            buttonLink={intl.formatMessage({
+              id: "student.callToAction.buttonLink",
+            })}
+            isSection={false}
+          />
+        </div>
+
+        <GoogleMaps />
+
+        <PyramideSection />
+
+        <CertificatesSection />
+
+        <Testimonials />
+
+        <section id="contact" className="pt-20 bg-primary text-white">
+          <div className="flex flex-col items-center">
+            <div className="container mx-auto px-4 max-w-6xl">
+              {/* Section Title */}
+              <h2 className="text-3xl md:text-4xl font-bold mb-12 text-white text-center">
+                {intl.formatMessage({ id: "student.contact.title" })}
+              </h2>
+
+              {/* Card Container - Centered */}
+              <div className="flex justify-center">
+                <ContactCard
+                  name={contactPerson.name}
+                  title={contactPerson.title}
+                  imageUrl={contactPerson.imageUrl}
+                  email={contactPerson.email}
+                  linkedinUrl={contactPerson.linkedinUrl}
+                  greeting={intl.formatMessage({
+                    id: "student.contact.greeting",
+                  })}
+                  variant="quote"
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
