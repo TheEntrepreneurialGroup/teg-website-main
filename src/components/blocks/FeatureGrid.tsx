541ffc93b2fc61d7e9e19017560d56b5e3da524a
Richard Magula <richard.magula@tum.de>
2025-12-08 22:04:07 +0100

    Improved why TEG section scaling behaviour: items now centered

--- src/components/blocks/FeatureGrid.tsx
+++ src/components/blocks/FeatureGrid.tsx
@@ -6,5 +6,5 @@ interface FeatureGridProps {
 }
 
 export function FeatureGrid({ children }: FeatureGridProps) {
-  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">{children}</div>
+  return <div className="w-full flex flex-wrap justify-center">{children}</div>
 }
