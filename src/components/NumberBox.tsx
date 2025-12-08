17685099a2f21dd35edcfd5cf9a1711a62d46f23
wmarcos15 <guillermoml9031@gmail.com>
2025-12-08 21:01:53 +0100

    Add TEG in Zahlen

---
+++ src/components/NumberBox.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface NumberBoxProps {
+    number: string;
+    text: string;
+}
+
+const NumberBox: React.FC<NumberBoxProps> = ({
+    number,
+    text
+}) => {
+    return (
+        <div className="bg-primary-light flex flex-col justify-around items-center w-64 h-64 p-2 pr-12 pl-12 rounded-lg">
+            <div className="text-gray-100 text-5xl text-center">{number}</div>
+            <div className="text-gray-100 text-center text-lg">{text}</div>
+        </div>
+    );
+}
+
+export default NumberBox;
