01e58493683ad59bf49fc3661454b5f26bb08b52
Richard Magula <richard.magula@tum.de>
2025-12-08 21:19:20 +0100

    Fixed full bleed not working properly

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -8,7 +8,7 @@ interface LandingPageProps {
 export function LandingPage({ children }: LandingPageProps) {
   return (
     <div className="min-h-screen bg-background">
-      <div className="max-w-[1400px] mx-auto px-4 md:px-6 lg:px-8 overflow-x-hidden">{children}</div>
+      <div className="max-w-[1400px] mx-auto px-4 md:px-6 lg:px-8 ">{children}</div>
     </div>
   )
 }
