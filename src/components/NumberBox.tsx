a4004546d0beaa0f30046e84dff852bbb67088c8
Richard Magula <richard.magula@tum.de>
2025-12-09 15:18:46 +0100

    forgot to save merge conflict resolution ...

--- src/components/NumberBox.tsx
+++ src/components/NumberBox.tsx
@@ -10,15 +10,9 @@ const NumberBox: React.FC<NumberBoxProps> = ({
     text
 }) => {
     return (
-<<<<<<< HEAD
         <div className="bg-primary-light flex flex-col justify-around items-center w-64 h-64 p-4 md:px-12 rounded-2xl">
             <div className="text-gray-100 text-5xl text-center">{number}</div>
             <div className="text-gray-100 text-center text-lg">{text}</div>
-=======
-        <div className="bg-primary-light w-60 h-60 flex-shrink-0 p-4 rounded-2xl">
-            <div className="text-gray-100 flex h-1/2 items-center justify-center text-5xl font-medium text-center">{number}</div>
-            <div className="text-gray-100 flex h-1/2 items-center justify-center text-center text-lg">{text}</div>
->>>>>>> 525a5e0 (TEG in Zahlen visially improved alignment and mobile ready)
         </div>
     );
 }
