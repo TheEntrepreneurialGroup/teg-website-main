ca925545213150729821ff9767ec2cf58f3ce321
lennart <lennart.klemm@tum.de>
2025-12-23 13:35:59 +0100

    minor changes, added picture

--- src/components/NewHeroTwoButtons.tsx
+++ src/components/NewHeroTwoButtons.tsx
@@ -32,12 +32,12 @@ const NewHeroTwoButtons: React.FC<NewHeroTwoButtonsProps> = ({
                 alt="Hero"
                 className="absolute inset-0 w-full h-full object-cover z-0"
             />
-            <div className="absolute inset-0 bg-primary-dark/80 z-10"></div>
+            <div className="absolute inset-0 bg-primary-dark/65 z-10"></div>
             <div className="absolute bottom-0 left-0 p-2 md:p-16 w-full">
                 <div className="relative z-20 h-full w-full flex flex-col justify-center items-center md:justify-start md:items-start">
                     <div className="flex flex-col justify-start w-full">
                         <h1 className="hidden sm:block text-left text-gray-100 mb-2 w-full">{title}</h1>
-                        <h1 className="block sm:hidden text-left text-gray-100 mb-2 w-full text-2xl">{mobileTitle}</h1> 
+                        <h1 className="block sm:hidden text-left text-gray-100 mb-2 w-full text-2xl">{mobileTitle}</h1>
                         <h3 className="text-left text-gray-100 font-normal w-full">{subtitle}</h3>
                     </div>
                     <div className="mt-4 md:mt-16 flex flex-col md:flex-row justify-center items-center md:justify-start md:items-start w-full">
