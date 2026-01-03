33033d3458d22e9e1d9c06fc2165a6f6b3c28dc7
Luis Waller <luis.waller@tum.de>
2026-01-03 20:30:35 +0100

    fix: formating + linting in Main

--- src/renderer/+onRenderClient.tsx
+++ src/renderer/+onRenderClient.tsx
@@ -1,8 +1,9 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function render({ Page, pageProps }: { Page: any; pageProps: any }) {
-  const container = document.getElementById('root');
-  if (!container) throw new Error('DOM element #root not found');
+  const container = document.getElementById("root");
+  if (!container) throw new Error("DOM element #root not found");
   const root = createRoot(container);
   root.render(<Page {...pageProps} />);
 }
