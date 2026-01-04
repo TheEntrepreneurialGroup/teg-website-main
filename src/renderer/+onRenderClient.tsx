2462ece38ab31c4c795402b8fee60b0a951e01f6
Luis Waller <luis.waller@tum.de>
2026-01-04 18:20:34 +0100

    delete: old renderer

--- src/renderer/+onRenderClient.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-export function render({ Page, pageProps }: { Page: any; pageProps: any }) {
-  const container = document.getElementById('root');
-  if (!container) throw new Error('DOM element #root not found');
-  const root = createRoot(container);
-  root.render(<Page {...pageProps} />);
-}
