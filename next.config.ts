9f6749d704426089b8498752cd39ba6e5f6531a0
Luis Waller <luis.waller@tum.de>
2026-01-24 22:41:11 +0100

    add: typescript next-intl enhancement

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import { createNextIntlPlugin } from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
 };
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin({
+  experimental: {
+    // Provide the path to the messages that you're using in `AppConfig`
+    createMessagesDeclaration: ["./messages/en.json", "./messages/de.json"],
+  },
+});
 
 export default withNextIntl(nextConfig);
