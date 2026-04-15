a505916b0ae72867ea4c62a628f1f176cecf861b
Luis Waller <luis.waller@tum.de>
2026-04-15 17:17:09 +0200

    align: call to action length

--- src/pages/ForStudents.tsx
+++ src/pages/ForStudents.tsx
@@ -81,7 +81,7 @@ const ForStudents: React.FC = () => {
           description={intl.formatMessage({ id: "home.alumnis.description" })}
         />
 
-        <div className="container-custom max-w-7xl mx-auto px-1 md:px-4 pb-8">
+        <div className="max-w-7xl mx-auto p-2 sm:p-8 md:p-14 lg:p-20">
           <CallToAction
             title={intl.formatMessage({ id: "student.callToAction.title" })}
             description={intl.formatMessage({
