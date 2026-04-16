6e08b1b864cbb96cc5336ac40b1855c6827c636b
Luis Waller <luis.waller@tum.de>
2026-04-16 12:48:43 +0200

    refactor: student title left aligned + testimonail spacing

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -6,9 +6,13 @@ interface SectionTitleProps {
   centered?: boolean;
 }
 
-const SectionTitle: React.FC<SectionTitleProps> = ({ title, subtitle }) => {
+const SectionTitle: React.FC<SectionTitleProps> = ({
+  title,
+  subtitle,
+  centered = true,
+}) => {
   return (
-    <div className={`mb-12`}>
+    <div className={`sm:mb-12 ${centered ? "text-center" : "text-left"}`}>
       <h2
         className="mb-4"
         initial={{ opacity: 0, y: 20 }}
--- src/components/ui/SingleTestimonial.tsx
+++ src/components/ui/SingleTestimonial.tsx
@@ -13,28 +13,26 @@ const SingleTestimonial: React.FC<SingleTestimonialProps> = ({
   image,
   reversed = false,
 }) => (
-  <div className="">
-    <div
-      className={[
-        "flex items-center gap-2 md:gap-x-4",
-        reversed ? "flex-row-reverse" : "flex-row",
-      ].join(" ")}
-    >
-      <img
-        src={image}
-        alt={author}
-        className="h-24 w-24 shrink-0 md:h-28 md:w-28 rounded-full object-cover"
-      ></img>
-      <div className="max-w-2xl">
-        <p className="text-xl italic text-gray-700 font-normal leading-relaxed m-0">
-          “{quote}”
-        </p>
-        <p className="text-xl not-italic text-primary-dark font-semibold mt-1 m-0">
-          {author}
-        </p>
-      </div>
+  <article
+    className={[
+      "flex items-center gap-2 md:gap-x-4",
+      reversed ? "flex-row-reverse" : "flex-row",
+    ].join(" ")}
+  >
+    <img
+      src={image}
+      alt={author}
+      className="h-24 w-24 shrink-0 md:h-28 md:w-28 rounded-full object-cover"
+    ></img>
+    <div className="max-w-2xl">
+      <p className="text-xl italic text-gray-700 font-normal leading-relaxed m-0">
+        “{quote}”
+      </p>
+      <p className="text-xl not-italic text-primary-dark font-semibold mt-1 m-0">
+        {author}
+      </p>
     </div>
-  </div>
+  </article>
 );
 
 export default SingleTestimonial;
